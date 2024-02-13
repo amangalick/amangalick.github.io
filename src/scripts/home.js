f59de6ba70a848c4c70a6d2f950a3739e27d22d5
meaganvu <124109605+meaganvu@users.noreply.github.com>
2024-02-13 12:57:21 -0800

    1

--- src/scripts/home.js
+++ src/scripts/home.js
@@ -90,6 +90,23 @@ function handleFormSubmission() {
         "1/2",
       ),
     );
+
+    content.appendChild(
+      TEST(
+        milesDriven,
+        smartPhonesCharged,
+        mdDesc,
+        spDesc,
+        carIconSrc,
+        phoneIconSrc,
+        "30px",
+        "18px",
+        "4/5",
+        "1/2",
+      ),
+    );
+
+
     // desktop computer
     if (window.screen.width >= 768) {
       content.appendChild(
@@ -167,35 +184,35 @@ function handleFormSubmission() {
   }
 }
 
-function toggleAR() {
-  // If device is mobile, open AR website in new tab (https://ytxxc.zappar.io/7543435830527519324/)
-  // Else, open AR QR code website in popup modal
+// function toggleAR() {
+//   // If device is mobile, open AR website in new tab (https://ytxxc.zappar.io/7543435830527519324/)
+//   // Else, open AR QR code website in popup modal
 
-  // Regex function provided by http://detectmobilebrowsers.com/
-  let check = false;
-  (function (a) {
-    if (
-      /(android|bb\d+|meego).+mobile|avantgo|bada\/|blackberry|blazer|compal|elaine|fennec|hiptop|iemobile|ip(hone|od)|iris|kindle|lge |maemo|midp|mmp|mobile.+firefox|netfront|opera m(ob|in)i|palm( os)?|phone|p(ixi|re)\/|plucker|pocket|psp|series(4|6)0|symbian|treo|up\.(browser|link)|vodafone|wap|windows ce|xda|xiino/i.test(
-        a,
-      ) ||
-      /1207|6310|6590|3gso|4thp|50[1-6]i|770s|802s|a wa|abac|ac(er|oo|s\-)|ai(ko|rn)|al(av|ca|co)|amoi|an(ex|ny|yw)|aptu|ar(ch|go)|as(te|us)|attw|au(di|\-m|r |s )|avan|be(ck|ll|nq)|bi(lb|rd)|bl(ac|az)|br(e|v)w|bumb|bw\-(n|u)|c55\/|capi|ccwa|cdm\-|cell|chtm|cldc|cmd\-|co(mp|nd)|craw|da(it|ll|ng)|dbte|dc\-s|devi|dica|dmob|do(c|p)o|ds(12|\-d)|el(49|ai)|em(l2|ul)|er(ic|k0)|esl8|ez([4-7]0|os|wa|ze)|fetc|fly(\-|_)|g1 u|g560|gene|gf\-5|g\-mo|go(\.w|od)|gr(ad|un)|haie|hcit|hd\-(m|p|t)|hei\-|hi(pt|ta)|hp( i|ip)|hs\-c|ht(c(\-| |_|a|g|p|s|t)|tp)|hu(aw|tc)|i\-(20|go|ma)|i230|iac( |\-|\/)|ibro|idea|ig01|ikom|im1k|inno|ipaq|iris|ja(t|v)a|jbro|jemu|jigs|kddi|keji|kgt( |\/)|klon|kpt |kwc\-|kyo(c|k)|le(no|xi)|lg( g|\/(k|l|u)|50|54|\-[a-w])|libw|lynx|m1\-w|m3ga|m50\/|ma(te|ui|xo)|mc(01|21|ca)|m\-cr|me(rc|ri)|mi(o8|oa|ts)|mmef|mo(01|02|bi|de|do|t(\-| |o|v)|zz)|mt(50|p1|v )|mwbp|mywa|n10[0-2]|n20[2-3]|n30(0|2)|n50(0|2|5)|n7(0(0|1)|10)|ne((c|m)\-|on|tf|wf|wg|wt)|nok(6|i)|nzph|o2im|op(ti|wv)|oran|owg1|p800|pan(a|d|t)|pdxg|pg(13|\-([1-8]|c))|phil|pire|pl(ay|uc)|pn\-2|po(ck|rt|se)|prox|psio|pt\-g|qa\-a|qc(07|12|21|32|60|\-[2-7]|i\-)|qtek|r380|r600|raks|rim9|ro(ve|zo)|s55\/|sa(ge|ma|mm|ms|ny|va)|sc(01|h\-|oo|p\-)|sdk\/|se(c(\-|0|1)|47|mc|nd|ri)|sgh\-|shar|sie(\-|m)|sk\-0|sl(45|id)|sm(al|ar|b3|it|t5)|so(ft|ny)|sp(01|h\-|v\-|v )|sy(01|mb)|t2(18|50)|t6(00|10|18)|ta(gt|lk)|tcl\-|tdg\-|tel(i|m)|tim\-|t\-mo|to(pl|sh)|ts(70|m\-|m3|m5)|tx\-9|up(\.b|g1|si)|utst|v400|v750|veri|vi(rg|te)|vk(40|5[0-3]|\-v)|vm40|voda|vulc|vx(52|53|60|61|70|80|81|83|85|98)|w3c(\-| )|webc|whit|wi(g |nc|nw)|wmlb|wonu|x700|yas\-|your|zeto|zte\-/i.test(
-        a.substr(0, 4),
-      )
-    )
-      check = true;
-  })(navigator.userAgent || navigator.vendor || window.opera);
+//   // Regex function provided by http://detectmobilebrowsers.com/
+//   let check = false;
+//   (function (a) {
+//     if (
+//       /(android|bb\d+|meego).+mobile|avantgo|bada\/|blackberry|blazer|compal|elaine|fennec|hiptop|iemobile|ip(hone|od)|iris|kindle|lge |maemo|midp|mmp|mobile.+firefox|netfront|opera m(ob|in)i|palm( os)?|phone|p(ixi|re)\/|plucker|pocket|psp|series(4|6)0|symbian|treo|up\.(browser|link)|vodafone|wap|windows ce|xda|xiino/i.test(
+//         a,
+//       ) ||
+//       /1207|6310|6590|3gso|4thp|50[1-6]i|770s|802s|a wa|abac|ac(er|oo|s\-)|ai(ko|rn)|al(av|ca|co)|amoi|an(ex|ny|yw)|aptu|ar(ch|go)|as(te|us)|attw|au(di|\-m|r |s )|avan|be(ck|ll|nq)|bi(lb|rd)|bl(ac|az)|br(e|v)w|bumb|bw\-(n|u)|c55\/|capi|ccwa|cdm\-|cell|chtm|cldc|cmd\-|co(mp|nd)|craw|da(it|ll|ng)|dbte|dc\-s|devi|dica|dmob|do(c|p)o|ds(12|\-d)|el(49|ai)|em(l2|ul)|er(ic|k0)|esl8|ez([4-7]0|os|wa|ze)|fetc|fly(\-|_)|g1 u|g560|gene|gf\-5|g\-mo|go(\.w|od)|gr(ad|un)|haie|hcit|hd\-(m|p|t)|hei\-|hi(pt|ta)|hp( i|ip)|hs\-c|ht(c(\-| |_|a|g|p|s|t)|tp)|hu(aw|tc)|i\-(20|go|ma)|i230|iac( |\-|\/)|ibro|idea|ig01|ikom|im1k|inno|ipaq|iris|ja(t|v)a|jbro|jemu|jigs|kddi|keji|kgt( |\/)|klon|kpt |kwc\-|kyo(c|k)|le(no|xi)|lg( g|\/(k|l|u)|50|54|\-[a-w])|libw|lynx|m1\-w|m3ga|m50\/|ma(te|ui|xo)|mc(01|21|ca)|m\-cr|me(rc|ri)|mi(o8|oa|ts)|mmef|mo(01|02|bi|de|do|t(\-| |o|v)|zz)|mt(50|p1|v )|mwbp|mywa|n10[0-2]|n20[2-3]|n30(0|2)|n50(0|2|5)|n7(0(0|1)|10)|ne((c|m)\-|on|tf|wf|wg|wt)|nok(6|i)|nzph|o2im|op(ti|wv)|oran|owg1|p800|pan(a|d|t)|pdxg|pg(13|\-([1-8]|c))|phil|pire|pl(ay|uc)|pn\-2|po(ck|rt|se)|prox|psio|pt\-g|qa\-a|qc(07|12|21|32|60|\-[2-7]|i\-)|qtek|r380|r600|raks|rim9|ro(ve|zo)|s55\/|sa(ge|ma|mm|ms|ny|va)|sc(01|h\-|oo|p\-)|sdk\/|se(c(\-|0|1)|47|mc|nd|ri)|sgh\-|shar|sie(\-|m)|sk\-0|sl(45|id)|sm(al|ar|b3|it|t5)|so(ft|ny)|sp(01|h\-|v\-|v )|sy(01|mb)|t2(18|50)|t6(00|10|18)|ta(gt|lk)|tcl\-|tdg\-|tel(i|m)|tim\-|t\-mo|to(pl|sh)|ts(70|m\-|m3|m5)|tx\-9|up(\.b|g1|si)|utst|v400|v750|veri|vi(rg|te)|vk(40|5[0-3]|\-v)|vm40|voda|vulc|vx(52|53|60|61|70|80|81|83|85|98)|w3c(\-| )|webc|whit|wi(g |nc|nw)|wmlb|wonu|x700|yas\-|your|zeto|zte\-/i.test(
+//         a.substr(0, 4),
+//       )
+//     )
+//       check = true;
+//   })(navigator.userAgent || navigator.vendor || window.opera);
 
-  if (check) {
-    window.open("https://ytxxc.zappar.io/7543435830527519324/", "_blank");
-  } else {
-    document.getElementById("overlay-modal").classList.toggle("invisible");
-    document.getElementById("popup-modal").classList.toggle("invisible");
-    document.getElementById("overlay-modal").classList.toggle("opacity-0");
-    document.getElementById("overlay-modal").classList.toggle("opacity-100");
-    document.getElementById("popup-modal").classList.toggle("opacity-0");
-    document.getElementById("popup-modal").classList.toggle("opacity-100");
-  }
-}
+//   if (check) {
+//     window.open("https://ytxxc.zappar.io/7543435830527519324/", "_blank");
+//   } else {
+//     document.getElementById("overlay-modal").classList.toggle("invisible");
+//     document.getElementById("popup-modal").classList.toggle("invisible");
+//     document.getElementById("overlay-modal").classList.toggle("opacity-0");
+//     document.getElementById("overlay-modal").classList.toggle("opacity-100");
+//     document.getElementById("popup-modal").classList.toggle("opacity-0");
+//     document.getElementById("popup-modal").classList.toggle("opacity-100");
+//   }
+// }
 
 function formValidation() {
   var alert =
@@ -744,4 +761,158 @@ function loadSheets() {
         );
       },
     );
+}
+
+function TEST1(
+  statistic,
+  description,
+  iconSrc,
+  iconSize = "30px",
+  width = "4/5",
+  desktopWidth = "3/12",
+) {
+  // Rewrite statistic with a K if over 1000
+  if (statistic > 1000) {
+    statistic = (statistic / 1000).toFixed(2) + "K";
+  }
+
+  var container = document.createElement("div");
+  container.classList.add(
+    "p-8",
+    "border",
+    "border-gray-300",
+    "rounded-3xl",
+    `w-${width}`,
+    `md:w-${desktopWidth}`,
+    "mx-auto",
+    "mt-2",
+    "shadow-md",
+    "bg-green-100",
+  );
+
+  var stat = document.createElement("p");
+  stat.classList.add(
+    "font-heading",
+    "mb-6",
+    "text-3xl",
+    "md:text-5xl",
+    "text-anr-off-blue",
+    "font-black",
+    "tracking-tight",
+    "max-w-full",
+  );
+  stat.innerHTML = statistic;
+
+  var desc = document.createElement("span");
+  desc.classList.add(
+    "font-heading",
+    "mb-2",
+    "text-base",
+    "text-gray-700",
+    "font-bold",
+  );
+
+  var icon = document.createElement("img");
+  icon.classList.add("inline");
+  icon.src = iconSrc;
+  icon.style.height = iconSize;
+  icon.alt = "icon";
+
+  desc.appendChild(icon);
+  desc.innerHTML += " " + description;
+
+  container.appendChild(stat);
+  container.appendChild(desc);
+
+  return container;
+}
+
+// function CO2StatBox(
+//   statistic,
+//   width = "4/5",
+//   desktopWidth = "3/12",
+// ) {
+//   // Rewrite statistic with a K if over 1000
+//   if (statistic > 1000) {
+//     statistic = (statistic / 1000).toFixed(2) + "K";
+//   }
+
+//   var container = document.createElement("div");
+//   container.classList.add(
+//     "p-8",
+//     "border",
+//     "border-gray-300",
+//     "rounded-3xl",
+//     `w-${width}`,
+//     `md:w-${desktopWidth}`,
+//     "mx-auto",
+//     "mt-2",
+//     "shadow-md",
+//     "bg-green-100",
+//   );
+
+//   var desc = document.createElement("span");
+//   desc.classList.add(
+//     "font-heading",
+//     "mb-7",
+//     "text-base",
+//     "text-gray-700",
+//     "font-bold",
+//   );
+
+//   let description = "You're Composting Efforts amounted to"
+//   desc.innerHTML += description;
+
+//   var stat = document.createElement("p");
+//   stat.classList.add(
+//     "font-heading",
+//     // "mb-2",
+//     "text-3xl",
+//     "md:text-3xl",
+//     "text-anr-off-blue",
+//     "font-black",
+//     "tracking-tight",
+//     "max-w-full",
+//   );
+//   stat.innerHTML = statistic + " kg of CO2";
+
+//   container.appendChild(desc);
+//   container.appendChild(stat);
+
+//   return container;
+// }
+
+
+function TEST(
+  stat1,
+  stat2,
+  desc1,
+  desc2,
+  icon1,
+  icon2,
+  icon1Sz = "30px",
+  icon2Sz = "30px",
+  mobileWidth = "4/5",
+  desktopWidth = "3/12",
+) {
+  var container = document.createElement("div");
+  container.classList.add(
+    "flex",
+    `w-${mobileWidth}`,
+    `md:w-${desktopWidth}`,
+    "mx-auto",
+    "bg-red-100",
+  );
+
+  var statCont1 = TEST1(stat1, desc1, icon1, icon1Sz);
+  statCont1.classList.remove("md:w-3/12","w-4/5"); //,"w-4/5"
+  statCont1.classList.add("mr-1","md:w-1/8", "w-1/3");
+  var statCont2 = TEST1(stat2, desc2, icon2, icon2Sz);
+  statCont2.classList.remove("md:w-3/12","w-4/5", "p-8");
+  statCont2.classList.add("ml-1","md:w-1/8","max-w-full", "w-2/3", "py-4");
+
+  container.appendChild(statCont1);
+  container.appendChild(statCont2);
+
+  return container;
 }
